a2017bc4d79f5e8d71df8fd8c310bc747083e1bc
ved2805 <vedrecharla@gmail.com>
2022-11-22 01:35:59 +0530

    server commit 5  for login

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ app.post("/login", (req, res)=> {
     User.findOne({ email: email}, (err, user) => {
         if(user){
             if(password === user.password ) {
-                res.send({message: "Login Successfull", user: user})
+                res.status(200).send({message: "Login Successfull", user: user})
             } else {
-                res.send({ message: "Password didn't match"})
+                res.status(200).send({ message: "Password didn't match"})
             }
         } else {
-            res.send({message: "User not registered"})
+            res.status(200).send({message: "User not registered"})
         }
     })
 }) 
@@ -43,7 +43,7 @@ app.post("/register", (req, res)=> {
     const { name, email, password} = req.body
     User.findOne({email: email}, (err, user) => {
         if(user){
-            res.send({message: "User already registerd"})
+            res.status(200).send({message: "User already registerd"})
         } else {
             const user = new User({
                 name,
@@ -54,7 +54,7 @@ app.post("/register", (req, res)=> {
                 if(err) {
                     res.send(err)
                 } else {
-                    res.send( { message: "Successfully Registered, Please login now." })
+                    res.status(200).send( { message: "Successfully Registered, Please login now." })
                 }
             })
         }
