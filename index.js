0f51d0a820bdc1e6e67b276f6578f704fdd6cbb1
ved2805 <vedrecharla@gmail.com>
2022-11-22 02:04:02 +0530

    first23  commit

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express();
 const port = 9002;
 app.use(express.json())
 app.use(express.urlencoded())
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 res.setHeader('Access-Control-Allow-Origin','*');
 res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
 res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
